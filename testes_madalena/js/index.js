e727c8c3a3679f1e7980c0cf878878cfbc95adc4
MadalenaC <mconceicao@student.dei.uc.pt>
2025-12-15 11:13:24 +0000

    added warning to the page

--- testes_madalena/js/index.js
+++ testes_madalena/js/index.js
@@ -1,5 +1,15 @@
+
+//LIMITS
 const initial_limit=window.innerWidth;
 const final_limit=0;
+
+const initial_opacity=1;
+const final_opacity=0;
+
+//WARNING
+const warning = document.getElementById('warning');
+let current_opacity= initial_opacity;
+let target_ocpacity=initial_opacity;
 
 //CONTAINER
 const container = document.getElementById('first_line');
@@ -15,6 +25,7 @@ const buttons = document.getElementById('buttons');
 let currentX2 = -initial_limit;
 let targetX2 = -initial_limit;
 
+
 // Evento de scroll com o wheel
 window.addEventListener('wheel', (e) => {
     e.preventDefault(); //bloqueia o scroll normal da pagina
@@ -22,11 +33,12 @@ window.addEventListener('wheel', (e) => {
     const scrollAmount = e.deltaY;
     const newTargetX= targetX - scrollAmount * 0.7; // novo destino X
     const newTargetX2= targetX2 + scrollAmount * 0.7; // novo destino X
+    const newTarget_opacity= target_ocpacity - scrollAmount *0.7;
 
     //limite
     targetX= Math.min(Math.max(newTargetX,final_limit), initial_limit)
     targetX2= Math.min(Math.max(newTargetX2,-initial_limit), final_limit)
-
+    target_ocpacity=Math.min(Math.max(newTarget_opacity, -initial_opacity), final_opacity)
 }, { passive: false }); //permite usar prevent default
  
 
@@ -52,3 +64,11 @@ function animate2() {
 }
 
 animate2();
+
+function animate_warning(){
+    current_opacity += (target_ocpacity - current_opacity) * 0.09;
+    warning.style.opacity = current_opacity;
+    requestAnimationFrame(animate_warning); //chama se a si propria-loop infinito
+}
+
+animate_warning();
