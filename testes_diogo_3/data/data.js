97cdee57a4c86f07e31f8a64bb6aa85584c30223
uma_garrafa_de_azeite <diogofsilva@student.dei.uc.pt>
2025-12-16 22:52:47 +0000

    made changes

--- testes_diogo_3/data/data.js
+++ testes_diogo_3/data/data.js
@@ -6,6 +6,8 @@ for (const url of urls) {
     const image = document.createElement('img');
     image.src = url;
     document.body.append(image);
+    image.onload = () => console.log('✅ Imagem carregada:', url);
+    image.onerror = () => console.error('❌ Erro ao carregar:', url);
 }
 }
 
@@ -13,6 +15,6 @@ function onResponse(response) {
 return response.text();
 }
 
-fetch('links_url.txt')
+fetch('/testes_diogo_3/assets/links_url.txt')
 .then(onResponse)
 .then(onTextReady);
